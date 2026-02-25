8811d4257f28cef682a8a8e269ba17ee13116dea
balajimalliganesh <118103103+balajimalliganesh@users.noreply.github.com>
2026-02-25 12:55:36 +0530

    Create App.java

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,9 @@
+
+package com.example;
+public class App {
+    public static final String MESSAGE = "Hello from Test Build!";
+    public static void main(String[] args) {
+        System.out.println(MESSAGE);
+    }
+    public static String getMessage() { return MESSAGE; }
+}
